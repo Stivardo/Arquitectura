a5bb6896aae9fac6d618498cada88378c04eb06d
Stivardo <stiv_es@hotmail.com>
2024-10-13 18:30:39 -0500

    Agregar archivos de proyecto.

---
+++ ArquitecturaDelComputador/Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace ArquitecturaDelComputador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn_numeral = new System.Windows.Forms.Button();
+            this.btn_michi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn1.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Location = new System.Drawing.Point(48, 147);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(79, 62);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn3.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.Location = new System.Drawing.Point(218, 147);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(79, 62);
+            this.btn3.TabIndex = 1;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn2.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(133, 147);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(79, 62);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnA.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnA.ForeColor = System.Drawing.Color.White;
+            this.btnA.Location = new System.Drawing.Point(303, 147);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(79, 62);
+            this.btnA.TabIndex = 3;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = false;
+            this.btnA.Click += new System.EventHandler(this.btnA_Click);
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnB.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnB.ForeColor = System.Drawing.Color.White;
+            this.btnB.Location = new System.Drawing.Point(303, 215);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(79, 62);
+            this.btnB.TabIndex = 7;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = false;
+            this.btnB.Click += new System.EventHandler(this.btnB_Click_1);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn5.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.Location = new System.Drawing.Point(133, 215);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(79, 62);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn6.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.Location = new System.Drawing.Point(218, 215);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(79, 62);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn4.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.Location = new System.Drawing.Point(48, 215);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(79, 62);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnC.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC.ForeColor = System.Drawing.Color.White;
+            this.btnC.Location = new System.Drawing.Point(303, 283);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(79, 62);
+            this.btnC.TabIndex = 11;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn8.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.Location = new System.Drawing.Point(133, 283);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(79, 62);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn9.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.ForeColor = System.Drawing.Color.White;
+            this.btn9.Location = new System.Drawing.Point(218, 283);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(79, 62);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn7.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.Location = new System.Drawing.Point(48, 283);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(79, 62);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnD.Font = new System.Drawing.Font("Bookman Old Style", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnD.ForeColor = System.Drawing.Color.White;
+            this.btnD.Location = new System.Drawing.Point(303, 351);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(79, 62);
+            this.btnD.TabIndex = 15;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = false;
+            this.btnD.Click += new System.EventHandler(this.btnD_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn0.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.ForeColor = System.Drawing.Color.White;
+            this.btn0.Location = new System.Drawing.Point(133, 351);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(79, 62);
+            this.btn0.TabIndex = 14;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn_numeral
+            // 
+            this.btn_numeral.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn_numeral.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_numeral.ForeColor = System.Drawing.Color.White;
+            this.btn_numeral.Location = new System.Drawing.Point(218, 351);
+            this.btn_numeral.Name = "btn_numeral";
+            this.btn_numeral.Size = new System.Drawing.Size(79, 62);
+            this.btn_numeral.TabIndex = 13;
+            this.btn_numeral.Text = "#";
+            this.btn_numeral.UseVisualStyleBackColor = false;
+            this.btn_numeral.Click += new System.EventHandler(this.btn_numeral_Click);
+            // 
+            // btn_michi
+            // 
+            this.btn_michi.BackColor = System.Drawing.Color.CadetBlue;
+            this.btn_michi.Font = new System.Drawing.Font("Bookman Old Style", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_michi.ForeColor = System.Drawing.Color.White;
+            this.btn_michi.Location = new System.Drawing.Point(48, 351);
+            this.btn_michi.Name = "btn_michi";
+            this.btn_michi.Size = new System.Drawing.Size(79, 62);
+            this.btn_michi.TabIndex = 12;
+            this.btn_michi.Text = "*";
+            this.btn_michi.UseVisualStyleBackColor = false;
+            this.btn_michi.Click += new System.EventHandler(this.btn_michi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 36);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "INGRESE LA CONTRASEÑA";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.progressBar1.Location = new System.Drawing.Point(48, 464);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(334, 35);
+            this.progressBar1.TabIndex = 17;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBoxPassword.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(51, 82);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.ReadOnly = true;
+            this.textBoxPassword.Size = new System.Drawing.Size(329, 40);
+            this.textBoxPassword.TabIndex = 18;
+            this.textBoxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 431);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 24);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "INTENTOS:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(431, 511);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn_numeral);
+            this.Controls.Add(this.btn_michi);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn_numeral;
+        private System.Windows.Forms.Button btn_michi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ArquitecturaDelComputador/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace ArquitecturaDelComputador
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ArduinoUno;
+        string passwordBuffer = ""; // Buffer para almacenar la contraseña ingresada
+        int intentosFallidos = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoUno = new SerialPort();
+
+            ArduinoUno.PortName = "COM6";
+            ArduinoUno.BaudRate = 9600;
+            ArduinoUno.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            try
+            {
+                ArduinoUno.Open();
+                progressBar1.Maximum = 100;  // Establece el valor máximo del ProgressBar
+                progressBar1.Value = 0;      // Inicializa el ProgressBar en 0
+                MessageBox.Show("Puerto COM abierto correctamente.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Acceso denegado al puerto COM. Ejecuta como administrador.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al abrir el puerto COM: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Evento para recibir datos del Arduino
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = ArduinoUno.ReadLine();
+
+            this.Invoke(new Action(() =>
+            {
+                MessageBox.Show($"Datos recibidos: {data}");
+
+                if (data.Contains("Contraseña incorrecta"))
+                {
+                    intentosFallidos++;
+                    progressBar1.Value = Math.Min(intentosFallidos * 25, 100);  // Incrementa en pasos de 25 hasta 100
+                    MessageBox.Show($"Intentos fallidos: {intentosFallidos}, Progreso: {progressBar1.Value}%");
+                }
+                else if (data.Contains("Contraseña correcta"))
+                {
+                    intentosFallidos = 0;
+                    progressBar1.Value = 0;
+                    MessageBox.Show("Contraseña correcta, alarma desactivada");
+                }
+            }));
+        }
+
+        // Envía la contraseña al Arduino una vez que se ingresen los 4 dígitos
+        private void EnviarDatosSiCompleto()
+        {
+            if (passwordBuffer.Length == 4)
+            {
+                ArduinoUno.Write(passwordBuffer);  // Envía la contraseña completa al Arduino
+                MessageBox.Show($"Enviando contraseña: {passwordBuffer}");  // Depuración
+                passwordBuffer = "";  // Limpia el buffer de la contraseña
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoUno.IsOpen)
+            {
+                ArduinoUno.Close();
+                MessageBox.Show("Puerto COM cerrado correctamente.");
+            }
+        }
+
+        // Eventos para botones de números y letras
+        private void BotonPresionado(string valor)
+        {
+            passwordBuffer += valor;    // Agrega el valor presionado al buffer
+            textBoxPassword.Text += "*"; // Muestra asteriscos en el campo de texto
+            EnviarDatosSiCompleto();     // Revisa si ya se completó la contraseña
+        }
+
+        private void btn1_Click(object sender, EventArgs e) { BotonPresionado("1"); }
+        private void btn2_Click(object sender, EventArgs e) { BotonPresionado("2"); }
+        private void btn3_Click(object sender, EventArgs e) { BotonPresionado("3"); }
+        private void btnA_Click(object sender, EventArgs e) { BotonPresionado("A"); }
+        private void btn4_Click(object sender, EventArgs e) { BotonPresionado("4"); }
+        private void btn5_Click(object sender, EventArgs e) { BotonPresionado("5"); }
+        private void btn6_Click(object sender, EventArgs e) { BotonPresionado("6"); }
+        private void btn7_Click(object sender, EventArgs e) { BotonPresionado("7"); }
+        private void btn8_Click(object sender, EventArgs e) { BotonPresionado("8"); }
+        private void btn9_Click(object sender, EventArgs e) { BotonPresionado("9"); }
+        private void btn0_Click(object sender, EventArgs e) { BotonPresionado("0"); }
+        private void btn_michi_Click(object sender, EventArgs e) { BotonPresionado("*"); }
+        private void btn_numeral_Click(object sender, EventArgs e) { BotonPresionado("#"); }
+        private void btnC_Click(object sender, EventArgs e) { BotonPresionado("C"); }
+        private void btnD_Click(object sender, EventArgs e) { BotonPresionado("D"); }
+        private void btnB_Click_1(object sender, EventArgs e) { BotonPresionado("B"); }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Opcional: código que quieras ejecutar al cargar el formulario
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            // Opcional: código que quieras ejecutar al interactuar con el ProgressBar
+        }
+    }
+}
